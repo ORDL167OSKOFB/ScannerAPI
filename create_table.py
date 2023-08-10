8b97ffe89de10c17cf28dd80f803d7b27e283baf
cullenjohn <cullenjohn167@gmail.com>
2023-08-10 20:51:48 +0100

    remove flask for creation query

--- create_table.py
+++ create_table.py
@@ -1,34 +1,51 @@
 import os
 import pyodbc
-from flask import Flask, jsonify, request
-from Select_food import select_data
-from flask_cors import CORS
 import configparser
-
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://40268037flaskfrontend.azurewebsites.net"}})
 
 def get_connection_string():
     azure_db = os.environ.get('DB_CONNECTION_STRING')
+    
     if azure_db:
         return azure_db
-    
+
     config = configparser.ConfigParser()
     config.read('config.ini')
-    
+
     server = config['DATABASE']['server']
     db = config['DATABASE']['db']
     username = config['DATABASE']['user']
     password = config['DATABASE']['password']
     driver = config['DATABASE']['driver']
     port = config['DATABASE']['port']
-    
+
     return f'DRIVER={driver};SERVER={server};PORT={port};DATABASE={db};UID={username};PWD={password}'
 
 def create_connection():
     return pyodbc.connect(get_connection_string())
 
-def create_table(connection):
+connection = create_connection()
+
+def check_table_exists():
+    cursor = connection.cursor()
+    cursor.execute("""
+    SELECT * 
+    FROM INFORMATION_SCHEMA.TABLES 
+    WHERE TABLE_NAME = 'Foods'
+    """)
+    result = cursor.fetchone()
+    return result is not None
+
+def init_food():
+    message = ""
+    if not check_table_exists():
+        create_table()
+        message = "New Food Table has been created."
+    else:
+        message = "Food Table already exists."
+    connection.close()
+    print(message)
+
+def create_table():
     try:
         cursor = connection.cursor()
         create_table_query = """
@@ -45,30 +62,4 @@ def create_table(connection):
     except Exception as e:
         print("An error occurred:", e)
 
-def check_table_exists(connection):
-    cursor = connection.cursor()
-    cursor.execute("""
-    SELECT * 
-    FROM INFORMATION_SCHEMA.TABLES 
-    WHERE TABLE_NAME = 'Foods'
-    """)
-    result = cursor.fetchone()
-    return result is not None
-
-def init_food():
-    with create_connection() as connection:
-        if not check_table_exists(connection):
-            create_table(connection)
-            return "New Food Table has been created."
-        else:
-            return "Food Table already exists."
-
-db_init_result = init_food()
-print(db_init_result)
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-    
-    # Redeploy
-    # Redeploy
-    # Redeploy
+init_food()
